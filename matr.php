879d6d95d0f76b3b05575c2730e2ac8b60c7d778
unknown <altrans@abv.bg>
2020-03-04 18:11:05 +0200

    add solution

--- matr.php
+++ matr.php
@@ -1,9 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Matrix task</title>
+</head>
+
+<body>
 <?php
 echo "<table>";
 for ($row = 1; $row <= 9; $row++) {
   echo "<tr>";
   for ($col = 1; $col <= 9; $col++) {
-  if($col == 1) 
+  if($col == 1 || $row == 1) 
 	{ 
 		echo "<th>".$row *$col."</th>";
 	}
@@ -16,3 +23,5 @@ for ($row = 1; $row <= 9; $row++) {
 }
 echo "</table>";
 ?>
+</body>
+</html>
